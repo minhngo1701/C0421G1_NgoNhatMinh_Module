a9acf6c9b2a1b0dcbba3d39402493e6927a27c52
Ngô Nhật Minh <minhngo171997@gmail.com>
2021-05-04 17:24:12 +0700

    bai tap 7

--- _07_bien_kieu_du_lieu_toan_tu/thuc_hanh/bai1_3.js
+++
@@ -1,7 +0,0 @@
-let  a = prompt("nhập a", "")
-let b = prompt("nhập b","")
-if (a%b == 0){
-        alert("bội số của " + b + "là " + a);}
-    else{
-        alert("bội số của" + b + "không phải là " + a);
-}
