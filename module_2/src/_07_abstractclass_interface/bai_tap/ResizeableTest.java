d34760e01f9d4937b283a067a2ec9e7931f5e5f3
Ngô Nhật Minh <minhngo171997@gmail.com>
2021-06-11 18:32:25 +0700

    commit

--- module_2/src/_07_abstractclass_interface/bai_tap/ResizeableTest.java
+++ module_2/src/_07_abstractclass_interface/bai_tap/ResizeableTest.java
@@ -20,17 +20,19 @@ public class ResizeableTest {
 
         Resizeable[] resizeables = new Resizeable[3];
         resizeables[0] = new Circle(3.4);
-        resizeables[1] = new Rectangle(2,4);
+        resizeables[1] = new Rectangle(2, 4);
         resizeables[2] = new Square(3);
 
         for (Resizeable resizeable : resizeables) {
             resizeable.resize(3);
             if (resizeable instanceof Circle) {
-                System.out.println(((Circle) resizeable).getArea());
+                System.out.println("Area circle is: " + ((Circle) resizeable).getArea());
             } else if (resizeable instanceof Rectangle) {
-                System.out.println(((Rectangle) resizeable).getArea());
-            } else if (resizeable instanceof Square)
-                System.out.println((Square)resizeable.);
+                System.out.println("Area rectangle is: " + ((Rectangle) resizeable).getArea());
+            } else if (resizeable instanceof Square) {
+                System.out.println("Area square is: " + ((Square) resizeable).getArea());
+            } else {
+                System.out.println("Invalid");
             }
         }
     }
