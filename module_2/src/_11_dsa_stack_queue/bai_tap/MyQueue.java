a3e90443ee23b637a271c4b0cd29014df7ae131e
Ngô Nhật Minh <minhngo171997@gmail.com>
2021-06-20 17:14:16 +0700

    commit

--- module_2/src/_11_dsa_stack_queue/bai_tap/MyQueue.java
+++ module_2/src/_11_dsa_stack_queue/bai_tap/MyQueue.java
@@ -19,50 +19,50 @@ public class MyQueue {
     }
 
 
-    public static void enQueue(Queue  q, int value)
+    public static void enQueue(Queue q, int value)
     {
         Node  temp = new Node();
-        temp .data = value;
-        if (q .front ==  null)
-            q .front = temp;
+        temp.data = value;
+        if (q.front ==  null)
+            q.front = temp;
         else
-            q .rear .link = temp;
+            q.rear .link = temp;
 
-        q .rear = temp;
-        q .rear .link = q .front;
+        q.rear = temp;
+        q.rear .link = q .front;
     }
 
 
-    public static int deQueue(Queue  q)
+    public static int deQueue(Queue q)
     {
-        if (q .front ==  null)
+        if (q.front == null)
         {
-            System.out.printf ("Queue is empty");
+            System.out.printf("Queue is empty");
             return Integer.MIN_VALUE;
         }
 
 
         int value;
-        if (q .front == q .rear)
+        if (q.front == q .rear)
         {
-            value = q .front .data;
-            q .front =  null;
-            q .rear =  null;
+            value = q.front .data;
+            q.front =  null;
+            q.rear =  null;
         } else {
-            Node  temp = q .front;
-            value = temp .data;
-            q .front = q .front .link;
-            q .rear .link= q .front;
+            Node temp = q.front;
+            value = temp.data;
+            q.front = q.front .link;
+            q.rear.link= q.front;
         }
 
         return value ;
     }
 
-    static void displayQueue( Queue  q)
+    public static void displayQueue( Queue  q)
     {
-        Node  temp = q .front;
+        Node  temp = q.front;
         System.out.println("Elements in Circular Queue are: ");
-        while (temp.link != q .front)
+        while (temp.link != q.front)
         {
             System.out.println(temp.data);
             temp = temp.link;
