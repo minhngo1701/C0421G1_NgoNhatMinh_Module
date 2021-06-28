ca87f74c9bfe6a288e1158dc64cee5b68e3e6176
Ngô Nhật Minh <minhngo171997@gmail.com>
2021-06-28 20:21:22 +0700

    commit

---
+++ module_2/src/_16_io_text_file/bai_tap/CopyFileText.java
@@ -0,0 +1,47 @@
+package _16_io_text_file.bai_tap;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CopyFileText {
+    public List<String> readFile(String filePath) {
+        List<String> list = new ArrayList<>();
+        try {
+            FileReader fileReader = new FileReader(filePath);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null && !line.equals("")) {
+                list.add(line);
+                System.out.println(line);
+            }
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+    public void writeFile(String filePath, String line) {
+        try {
+            FileWriter fileWriter = new FileWriter(filePath, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(line);
+            bufferedWriter.newLine();
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        List<String> list;
+        CopyFileText copyFileText = new CopyFileText();
+        list = copyFileText.readFile("src\\_16_io_text_file\\thuc_hanh\\test\\file.txt");
+        System.out.println(list);
+        for (String n : list) {
+            copyFileText.writeFile("src\\_16_io_text_file\\thuc_hanh\\test\\newfile.txt", n);
+        }
+    }
+}
